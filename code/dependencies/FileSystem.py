6008fdf9e53a3b6c7cd931e9b6655d17493a042a
Gabriela <92589087+MarnieGrenat@users.noreply.github.com>
2023-10-27 10:52:40 -0300

    Add files via upload

--- code/dependencies/FileSystem.py
+++ code/dependencies/FileSystem.py
@@ -1,29 +1,16 @@
-import Debugger as debug
-
-
-def capturarAssembly(arquivo:str) -> bool:
-	commandsAssembly = ['or', 'and', 'sub', 'sltiu', 'lw', 'sw', 'beq', 'j']
-	commandsHex = []
-	raw_assembly = capturarCodigoAssembly()
-	assembly = gerarAssembly(raw_assembly)
-	salvaArquivo(assembly, "../hexadecimal/" + arquivo + "_MOUNTED"+ ".txt")
-	pass
-
+import FileSystem as fileSys
+from Debugger import *
 
 def getListOfFiles(path:str) -> list:
 	pass
 
-
-def InformacoesNoTerminal(countHexa:int, countAssembly:int) -> int:
+def printINFO_onTerminal(countHexa:int, countAssembly:int) -> int:
 	if	(not countAssembly and not countHexa):
-		print("Não foi gerado nenhum arquivo. Verifique se você adicionou o arquivo no local correto. Leia o manual em Documentacao")
+		logI("Não foi gerado nenhum arquivo. Verifique se você adicionou o arquivo no local correto. Leia o manual em Documentacao")
 	else:
-		print("Por favor, cheque as pastas para acessar os arquivos gerados!")
+		logI("Por favor, cheque as pastas para acessar os arquivos gerados!")
 	if (countHexa):
-		print(f"{countHexa} arquivo(s) hexa gerado(s).")
+		logI(f"{countHexa} arquivo(s) hexa gerado(s).")
 	if (countAssembly):
-		print(f"{countAssembly} arquivo(s) assembly gerado(s).")
+		logI(f"{countAssembly} arquivo(s) assembly gerado(s).")
 	return 0
-
-def saveFile(arquivo:str, path:str) -> bool:
-    return True
