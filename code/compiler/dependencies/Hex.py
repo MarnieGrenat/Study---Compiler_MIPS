a2e64aba7f852c2cfeb010d6049d051f741c99f1
Gabriela Dellamora Paim <gabriela@dellamora.dev>
2023-11-14 10:16:04 -0300

    starting hex dec

--- code/compiler/dependencies/Hex.py
+++ code/compiler/dependencies/Hex.py
@@ -1,27 +1,56 @@
+from Error import Error
+from Debugger import logD, logE, logI, logV, logW
 class Hex:
     def __init__(self, hexCode:str) -> None:
         self.binaryCode = self.generateBinary()
-        self.hexCode = hexCode.splitLines()
-        self.labels = self.findAllLabels()
-        
+        self.hexCode = hexCode
+        self.assembly = self.generateAssembly()
+
         def generateBinary(self) -> list:
             binary = []
             for hexLine in self.hexCode:
                 binary.append(self.hexToBin(hexLine))
             return binary
-        
+
         def hexToBin(line:str) -> str:
-            line = bin(int(line, 16))                   # hex -> dec -> bin
-            line = line[2:].zfill(32)                   # 32 bits only and remove 0b
+            line = bin(int(line, 16))
+            line = line[2:].zfill(32)
             return str(line)
-      
+
+        def generateAssembly(self) -> str:
+            opCodeBin = self.generateOpCode()
+            self.translateCommand(opCodeBin)
+
         ### GETTERS ###
-        def getBinaryCode(self) -> str:
+        def getBinaryCode(self) -> list:
             return self.binaryCode
 
         def getHexCode(self) -> str:
             return self.hexCode
 
+        def getAssemblyCode(self) -> str:
+            return self.assemblyCode
+
     ######################### TRADUÇÃO BINÁRIO PARA ASSEMBLY #########################
-    
-    
+
+        def getOpcode(self) -> str:
+            token = self.binaryCode[0:6]
+            return token
+
+        def translateCommand(token:str) -> str: #TODO: trocar values
+            opCodeMapping = {
+                'or': '0b000000',
+                'and': '0b000000',
+                'sub': '0b000000',
+
+                'beq': '0b000100',
+                'lw': '0b100011',
+                'sw': '0b101011',
+                'sltiu': '0b001011',
+
+                'j': '0b000010'
+            }
+            if token in opCodeMapping:
+                logV(f"OpCode encontrado no mapeamento. Retornando valor: {self.first6Bits(opCodeMapping[token])}")
+                return self.first6Bits(opCodeMapping[token])
+            raise Error(f"Comando {token} não reconhecido.")
