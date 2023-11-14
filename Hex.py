36e3e65dad78ffad8c9b7f488746b3d25b51aaa0
Gabriela <92589087+MarnieGrenat@users.noreply.github.com>
2023-11-14 16:08:41 -0300

    Add files via upload

--- Hex.py
+++ Hex.py
@@ -1,10 +1,13 @@
 from Error import Error
 from Debugger import logD, logE, logI, logV, logW
+
+
 class Hex:
-    def __init__(self, hexCode:str) -> None:
+    def __init__(self, hexCode: str) -> None:
         self.binaryCode = self.generateBinary()
         self.hexCode = hexCode
         self.assembly = self.generateAssembly()
+        self.labels = []
 
     def generateBinary(self) -> list:
         binary = []
@@ -12,7 +15,7 @@ class Hex:
             binary.append(self.hexToBin(hexLine))
         return binary
 
-    def hexToBin(line:str) -> str:
+    def hexToBin(line: str) -> str:
         line = bin(int(line, 16))
         line = line[2:].zfill(32)
         return str(line)
@@ -25,9 +28,8 @@ class Hex:
             assembly += "\n"
         return assembly
 
-
-
     ### GETTERS ###
+
     def getBinaryCode(self) -> list:
         return self.binaryCode
 
@@ -37,7 +39,7 @@ class Hex:
     def getAssemblyCode(self) -> str:
         return self.assemblyCode
 
-    def generateOpcode(self, binary:str) -> str:
+    def generateOpcode(self, binary: str) -> str:
         token = self.binary[0:6]
 
         opCodeMapping = {
@@ -53,11 +55,12 @@ class Hex:
             '000010': 'j'
         }
         if token in opCodeMapping:
-            logV(f"OpCode encontrado no mapeamento. Retornando valor: {self.first6Bits(opCodeMapping[token])}")
+            logV(
+                f"OpCode encontrado no mapeamento. Retornando valor: {self.first6Bits(opCodeMapping[token])}")
             return opCodeMapping[token]
         raise Error(f"Comando {token} não reconhecido.")
 
-    def translateCommand(self, opCode:str, binary:str) -> str:
+    def translateCommand(self, opCode: str, binary: str) -> str:
         if opCode in ['or', 'and', 'sub']:
             return self.translateRType(binary)
         if opCode in ['beq', 'lw', 'sw', 'sltiu']:
@@ -73,7 +76,7 @@ class Hex:
         line = f"{opCode}, {rs}, {rt}, {rd}"
         return line
 
-    def generateOpCodeR(self, binary:str):
+    def generateOpCodeR(self, binary: str):
         funct = binary[-6:]
         match funct:
             case '100101':
@@ -85,25 +88,90 @@ class Hex:
             case _:
                 raise Error(f"Função não conhecida: {funct}")
 
-    def generateRegister(self, binary:str, position:int) -> str:
+    def generateRegister(self, binary: str, position: int) -> str:
         registerBinary = self.getRegisterPosition(binary, position)
         return self.tokenizeRegister(registerBinary)
 
-    def getRegisterPosition(binary:str, position:int):
+    def getRegisterPosition(binary: str, position: int):
         match position:
             case 0:
-                return binary [7:11]
+                return binary[7:11]
             case 1:
-                return binary [11:16]
+                return binary[11:16]
             case 2:
-                return binary [16:21]
+                return binary[16:21]
 
-    def BinaryToRegister(self, register:str):
+    def tokenizeRegister(self, register: str) -> str:
         token = "$" + int(register, 2)
         return token
 
-    def generateOpCodeI(self, binary:str, CommandType:str):
+    def generateOpCodeI(self, binary: str, CommandType: str):
         if CommandType in ['lw', 'sw']:
             return self.generateBracketsFormat(binary)
         if CommandType in ['beq', 'sltiu']:
             return self.generateImmediateFormat(binary)
+
+    def generateBracketsFormat(self, binary: str) -> str:
+        '''OP rt,offset(rs)'''
+        opCode = self.getOpCodeI(binary[0:6])
+        rs = self.generateRegister(binary, 1)
+        rt = self.generateRegister(binary, 0)
+        offset = int(binary[16:], 2)
+        line = f"{opCode}, {rt}, {offset}({rs})"
+        return line
+
+    def generateImmediateFormat(self, binary) -> str:
+        '''OP rs,rt,imm'''
+        line = ''
+        opCode = self.getOpCodeI(binary[0:6])
+        rs = self.generateRegister(binary, 0)
+        rt = self.generateRegister(binary, 1)
+        imm = int(binary[16:], 2)
+
+        if opCode == 'beq':  # TODO: Gerar Label
+            self.generateLabel(binary[16:])
+            offset  = self.getImmediateRelativeAddr(binary[16:])
+            line = f"{opCode}, {rs}, {rt}, {offset}"
+
+        elif opCode == 'sltiu':
+            imm = int(imm, 2)
+            line = f"{opCode}, {rt}, {rs}, {imm}"
+        else:
+            raise Error(f"Comando não reconhecido.{binary}")
+
+        return line
+    def getImmediateRelativeAddr(self, binary: str) -> dict:
+        if binary[0] == '1':
+            binary = self.twosComplement(binary)
+        else:
+            labelName = 'L' + str(len(self.labelCounter))
+
+            return {labelName: binary}
+
+    def twosComplement(self, immediate: str) -> int:
+        '''ref: https://www.adamsmith.haus/python/answers/how-to-take-two's-complement-in-python'''
+        immediateBin = ''
+        for b in immediate:
+            if b == "0":
+                immediateBin += "1"
+            else:
+                immediateBin += "0"
+
+        immediate = int(immediateBin, 2)
+
+        immediate *=-1
+        immediate += 1
+        logI(f"immediate Complemento: {immediateBin}")
+        if immediate > 0:
+            raise Error(f"Valor inválido para operação de complemento de dois: {immediate}")
+        return immediateBin
+
+    def generateLabel(self, binary: str) -> str:
+        #TODO: Iterar sobre o arquivo final e gerar label
+        pass
+
+    def TranslateJType(self, binary):
+        opCode = self.getOpCodeJ(binary[0:6])
+        address = self.generateJLabel(int(binary[6:], 2))
+        return f"{opCode}, {address}"
+
