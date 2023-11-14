9d9d3125538c255f29dc6f8c3f24b4b0916a9dc8
Gabriela <92589087+MarnieGrenat@users.noreply.github.com>
2023-11-14 16:09:04 -0300

    Add files via upload

---
+++ Debugger.py
@@ -0,0 +1,32 @@
+# Modifique os valores de DEBUG_LEVEL para alterar o nível de debug.
+ERROR  = 1
+WARNING = 2
+INFO = 3
+DEBUG = 4
+VERBOSE = 5
+
+DEBUG_LEVEL = VERBOSE
+
+"""
+    Debugar o compilador. Possui 5 níveis de debug, sendo 1 o mais importante e 5 o menos importante.
+    Segue padrão ESP32.
+"""
+
+def debug(message:str, debug_level:int):
+    if (debug_level <= DEBUG_LEVEL):
+        print(message)
+
+def logE(message:str):
+    debug(message, ERROR)
+
+def logW(message:str):
+    debug(message, WARNING)
+
+def logI(message:str):
+    debug(message, INFO)
+
+def logD(message:str):
+    debug(message, DEBUG)
+
+def logV(message:str):
+    debug(message, VERBOSE)
